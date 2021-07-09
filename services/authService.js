5208744ab72cbed6886ed01b95b4814b9cfb19af
unknown <lucassodrem@gmail.com>
2021-07-09 17:58:37 -0300

    fix: Package bcrypt substituido para bcrypt.js.

--- services/authService.js
+++ services/authService.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/AppError');
 const db = require('../db');
